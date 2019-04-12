00657d71f4a55e8cae1117a6fc6c2421ae685e84
funkyl1zard <9975829+mvren@users.noreply.github.com>
2019-04-12 21:28:14 +0200

    (╯°□°)╯︵ ┻━┻

--- app/src/main/java/com/uist/arfurniture/MainActivity.kt
+++ app/src/main/java/com/uist/arfurniture/MainActivity.kt
@@ -2,40 +2,26 @@ package com.uist.arfurniture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MotionEvent
 import android.widget.Toast
 import com.google.ar.core.ArCoreApk
-import com.google.ar.core.Session
+import com.google.ar.core.HitResult
+import com.google.ar.core.Plane
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.Renderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var fragment: ArFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        enableAR();
-    }
 
-    override fun onResume() {
-        super.onResume()
-        if (!CameraPermissionHelper.hasCameraPermission(this)) {
-            CameraPermissionHelper.requestCameraPermission(this);
-            return;
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (!CameraPermissionHelper.hasCameraPermission(this)) {
-            Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
-                .show();
-            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
-                // Permission denied with checking "Do not ask again".
-                CameraPermissionHelper.launchPermissionSettings(this);
-            }
-            finish();
-        }
-    }
-
-    fun enableAR() {
-        var avail: ArCoreApk.Availability = ArCoreApk.getInstance().checkAvailability(this);
+        fragment = supportFragmentManager.findFragmentById(R.id.ar_fragment) as ArFragment
     }
 }
